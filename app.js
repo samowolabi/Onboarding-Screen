e5f37a696a0297ec8686cd82c4d0c2386c198a24
Samuel Owolabi <53657942+Samuel17715@users.noreply.github.com>
2022-08-18 20:24:34 +0100

    New

--- app.js
+++ app.js
@@ -111,6 +111,7 @@ const installPWAApp = async() => {
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
     const { outcome } = await deferredPrompt.userChoice;
+    console.log(outcome);
     // Optionally, send analytics event with outcome of user choice
     console.log(`User response to the install prompt: ${outcome}`);
     // We've used the prompt, and can't use it again, throw it away
