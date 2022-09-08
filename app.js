0209fc6d125ce9ae809daac3949749674e31e7c5
Samuel Owolabi <53657942+Samuel17715@users.noreply.github.com>
2022-09-09 00:34:32 +0100

    New

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ const isIos = () => {
 
 var displayMode = null;
 
+let hideInstallPromotionIOS = () => {
+    document.querySelector('.installPromotionDiv').innerHTML = ``;
+}
+
 let showInstallPromotionIOS = () => {
     let html = `
         <style>
@@ -54,7 +58,7 @@ let showInstallPromotionIOS = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" style="margin-bottom:-12px" fill="#4693FE" viewBox="0 0 50 50" enable-background="new 0 0 50 50"><path d="M30.3 13.7L25 8.4l-5.3 5.3-1.4-1.4L25 5.6l6.7 6.7z"/><path d="M24 7h2v21h-2z"/><path d="M35 40H15c-1.7 0-3-1.3-3-3V19c0-1.7 1.3-3 3-3h7v2h-7c-.6 0-1 .4-1 1v18c0 .6.4 1 1 1h20c.6 0 1-.4 1-1V19c0-.6-.4-1-1-1h-7v-2h7c1.7 0 3 1.3 3 3v18c0 1.7-1.3 3-3 3z"/></svg>
                     and then add to your homescreen.
                 </div>
-                <div style="margin-left: 0.25rem" onclick="() => {document.querySelector('.iosNotificationBarDiv').style.display='none'; alert();}">
+                <div style="margin-left: 0.25rem" onclick="hideInstallPromotionIOS()">
                     <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 1.5L22.8627 22.5627" stroke="#51526C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M22.8623 1.5L1.49961 22.5627" stroke="#51526C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>
                 </div>
             </div>
